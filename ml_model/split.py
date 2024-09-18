455585263efb07681bed4ab1398cde479fd3c381
cedTrad <cedtrad3@gmail.com>
2024-09-18 18:40:51 +0000

    nouveau modele

---
+++ ml_model/split.py
@@ -0,0 +1,39 @@
+import numpy as np
+from sklearn.model_selection import KFold
+
+class PurgedKFold(KFold):
+    def __init__(self, n_splits=5, embargo=0):
+        super(PurgedKFold, self).__init__(n_splits, shuffle=False, random_state=None)
+        self.embargo = embargo
+
+    def split(self, X, y=None, groups=None):
+        indices = np.arange(len(X))
+        test_starts = [(i[0], i[-1] + 1) for i in np.array_split(indices, self.n_splits)]
+        for start, end in test_starts:
+            t_ind = np.arange(start, end)
+            train_indices = np.concatenate([np.arange(0, start), np.arange(end, len(X))])
+            # Embargo applied here
+            if self.embargo > 0:
+                train_indices = np.setdiff1d(train_indices, np.arange(end, end + self.embargo))
+            yield train_indices, t_ind
+
+
+
+from sklearn.model_selection import KFold
+
+class PurgedKFold(KFold):
+    def __init__(self, n_splits=5, purge_length=1, embargo_length=1):
+        super().__init__(n_splits=n_splits, shuffle=False)
+        self.purge_length = purge_length
+        self.embargo_length = embargo_length
+
+    def split(self, X, y=None, groups=None):
+        indices = np.arange(len(X))
+        test_starts = [(i[0], i[-1] + 1) for i in np.array_split(np.arange(len(X)), self.n_splits)]
+
+        for i, j in test_starts:
+            purge_start = max(i - self.purge_length, 0)
+            purge_end = min(j + self.embargo_length, len(X))
+            train_indices = np.concatenate((indices[:purge_start], indices[purge_end:]))
+            test_indices = indices[i:j]
+            yield train_indices, test_indices
